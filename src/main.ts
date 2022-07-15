3e1485af381bab8c8ddb7721c2946cb272cd8990
xuyan <xuyan@sysftech.com>
2022-07-15 19:58:49 +0800

    init commit

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import AcroVue from '@arco-design/web-vue'
 import App from './App.vue'
 import '@arco-design/web-vue/dist/arco.css'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 app.use(AcroVue, {
     componentPrefix: 'arco'
-})
+}).use(createPinia())
 
 app.mount('#app')
